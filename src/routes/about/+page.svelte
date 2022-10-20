7c4960f3c7dd93b0cc8586a13ee0b28b0dfd5b2f
rlfx <rlfx@realfx.rocks>
2022-10-20 22:24:32 +0200

    rewrote about page to make it less suck-y

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,4 @@
 <script>
-	// wow, me making future plans for multiple languages??
-
-	let lang = {
-		hero: 'What is newTab?',
-		para1_1: 'newTab is my start tab, mostly just for me to write something.',
-		para1_2: "Don't expect alot.",
-		para2_1: 'If you want to know more,',
-		para2_2: 'Check the project out on '
-	};
 </script>
 
 <svelte:head>
@@ -16,29 +7,36 @@
 </svelte:head>
 
 <section>
-	<div class="about route">
-		<h1>{lang.hero}</h1>
-		<section class="section one">
-			<p>{lang.para1_1}</p>
-			<p>{lang.para1_2}</p>
-		</section>
-		<section class="section two">
-			<p>{lang.para2_1}</p>
-			<p>
-				{lang.para2_2}
-				<a href="https://github.com/RealFX-Code/newTab">GitHub</a>
-			</p>
-		</section>
+	<div class="about">
+		<h1>What is newTab?</h1>
+		<div class="text">
+			<p>newTab is my new tab bage for my web brosers. Kinda obvious really...
+				You've got four cards on the main page, where you click them and they take you to wherever they say they will.
+				And these tabs that you've found, They contain other stuff that I may find useful at any time.</p>
+			<p>If you have anything you'd like to say about this, or make a feature, or fix something, send me an issue or pull request at the
+				<a href="https://github.com/RealFX-Code/newtab">Github</a>
+				page.</p>
+		</div>
 	</div>
 </section>
 
 <style>
-	div.about h1 {
-		font-size: 10rem;
+	h1 {
+		font-size: 3rem;
 		font-weight: bolder;
 	}
-	div.about p {
-		font-size: 3rem;
-		font-weight: bold;
+	div.text {
+		margin: auto;
+		word-wrap: normal;
+		line-break: strict;
+		width: 75vw;
+	}
+	@media only screen and (max-width: 600px) {
+  		div.text {
+			width: 100%;
+		}
+	}
+	p {
+		font-size: 2rem;
 	}
 </style>
