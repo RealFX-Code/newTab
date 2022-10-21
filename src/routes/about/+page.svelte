4c2f9634b179df5f73cc0a757f307828e25bbbfe
rlfx <rlfx@realfx.rocks>
2022-10-21 21:40:33 +0200

    fix typo in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,10 +10,10 @@
 	<div class="about">
 		<h1>What is newTab?</h1>
 		<div class="text">
-			<p>newTab is my new tab bage for my web brosers. Kinda obvious really...
+			<p>newTab is my new tab page for my web brosers. Kinda obvious really...
 				You've got four cards on the main page, where you click them and they take you to wherever they say they will.
 				And these tabs that you've found, They contain other stuff that I may find useful at any time.</p>
-			<p>If you have anything you'd like to say about this, or make a feature, or fix something, send me an issue or pull request at the
+			<p>If you have anything you'd like to say about this, a feature, or want fix something, send me an issue or pull request at the
 				<a href="https://github.com/RealFX-Code/newtab">Github</a>
 				page.</p>
 		</div>
